4a841da1d9a307f743cfe2bae2698913722fb892
agthomal <agthomal@gmail.com>
2024-11-28 15:30:45 -0300

    Opcao Download All em UDP

--- UDP/client.cpp
+++ UDP/client.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <fstream>
 #include <filesystem>
+#include <chrono>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,9 +10,12 @@
 #include <string.h>
 
 using namespace std;
+using namespace std::chrono;
 
 #define LIST_FILE 1
 #define DOWNLOAD_FILE 2
+#define DOWNLOAD_ALL_FILES 3
+#define NEXT_FILE 5
 
 #define BUFF_SIZE 2048
 #define MAX_ARQ_NAME 1024
@@ -185,7 +189,96 @@ int main(int argc, char *argv[]){
                     }
                 }
             }
-            break;  
+            break;
+
+            case (DOWNLOAD_ALL_FILES):
+                {
+                packet.op = DOWNLOAD_ALL_FILES; 
+                
+                sendto(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &sa, sizeof(sa));
+                memset(&packet, 0, sizeof(struct network_packet));
+
+                bytes_rec = recvfrom(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &sa, &i);
+
+                auto list_start = high_resolution_clock::now();
+                while(packet.more != 0){ 
+                    
+                    char name[MAX_ARQ_NAME+1];
+                    char newfile[MAX_ARQ_NAME+1];
+
+                    strcpy(newfile, "client_arqs/");
+                    strcpy(name, packet.buf);
+                    strcat(newfile, packet.buf);
+                    
+                    ofstream file;
+                    file.open(newfile);
+
+                    int packet_count = 0;
+                    unsigned int bytes_left = packet.bytes;
+
+                    cout << "Baixando: " << name << endl;
+                    cout << "Tamanho: " << packet.bytes << endl;
+
+                    while(1){
+                        recvfrom(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &sa, &i);
+                        packet_count++;
+
+                        if (packet.op == NEXT_FILE || packet.more == 0) {
+                            break;
+                        }
+
+                        file.write(packet.buf, packet.bytes);
+
+                        bytes_left -= packet.bytes; 
+                        
+                        memset(&packet, 0, sizeof(struct network_packet));
+                    }
+
+                    file.close();
+
+                    cout << "Transmissão Concluida! Arquivo " << name << endl;
+                    cout << "Salvo em: " << newfile << endl;
+                    cout << packet_count << " packets received" << endl;
+
+                    if (argc == 4) {
+                        if (strcmp(argv[3], "-c") == 0) {
+                            char original_name[MAX_ARQ_NAME+1];;
+                            memset(original_name, 0 , MAX_ARQ_NAME + 1);
+                            strcpy(original_name, "server_arqs/");
+                            strcat(original_name, name);
+
+                            ifstream original;
+                            ifstream received;
+
+                            original.open(original_name);
+                            received.open(newfile);
+
+                            std::filesystem::path p_original{original_name};
+                            std::filesystem::path p_received{newfile};
+
+                            if (!received.fail()) {
+                                long int original_size = std::filesystem::file_size(p_original);
+                                long int received_size = std::filesystem::file_size(p_received);
+                                long int diff = original_size - received_size;
+                                int percentage = 100 * received_size / original_size;
+                                cout << "Um total de " << diff << " bytes não chegaram, sendo que cerca de " << percentage << "\% dos bytes foram transmitidos" << endl;
+                            }
+
+                            original.close();
+                            received.close();
+                        }
+                    }
+
+                    recvfrom(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &sa, &i);
+                }
+                auto list_stop = high_resolution_clock::now();
+                auto list_duration = duration_cast<microseconds>(list_stop-list_start);
+                
+                cout << "Todos os arquivos foram recebidos com sucesso!" << endl;
+                cout << "Tempo de transmissão de todos os arquivos: " << list_duration.count() << " ms" << endl;
+
+                }
+                break;
         }
 
         opt = chose_option();
--- UDP/server.cpp
+++ UDP/server.cpp
@@ -15,8 +15,12 @@
 
 #define LIST_FILE 1
 #define DOWNLOAD_FILE 2
+#define DOWNLOAD_ALL_FILES 3
+#define NEXT_FILE 4
+
 #define BUFF_SIZE 2048
 #define MAX_ARQ_NAME 1024
+#define PACK_ROOF 100
 
 #define END_OF_FILE "EOF"
 
@@ -119,6 +123,7 @@ int main(int argc, char *argv[]){
             break;
 
             case (DOWNLOAD_FILE):
+                {
                 char arq_name[MAX_ARQ_NAME + 1];
                 memset(arq_name, 0 , MAX_ARQ_NAME + 1);
                 strcpy(arq_name, "server_arqs/");
@@ -174,6 +179,98 @@ int main(int argc, char *argv[]){
                 cout << "Tempo de transmissão: " << duration.count() << " ms" << endl;
                 cout << packet_count << " pacotes enviados " << endl;
 
+                break;
+                }
+
+            case (DOWNLOAD_ALL_FILES):
+                {
+                const char *command = "find server_arqs -type f -printf \"%f\n\" > temp";
+        
+                system(command);
+
+                ifstream list_files("temp"); 
+
+                if(!list_files.is_open()){
+                    cout << "Erro ao abrir arquivo" << endl;
+                }
+                string file_name;
+                auto list_start =  high_resolution_clock::now();
+                while(getline(list_files, file_name)){
+                    
+                    cout << "Enviando: " << file_name << endl;
+
+                    char arq_name[MAX_ARQ_NAME + 1];
+                    memset(arq_name, 0 , MAX_ARQ_NAME + 1);
+                    strcpy(arq_name, "server_arqs/");
+                    memset(&packet, 0, sizeof(struct network_packet));
+
+                    file_name.copy(packet.buf, file_name.size());
+
+                    strcat(arq_name, packet.buf);
+
+                    unsigned int file_size = filesystem::file_size(arq_name);
+
+                    packet.op = DOWNLOAD_ALL_FILES;
+                    packet.bytes = file_size; 
+                    packet.more = 1;
+                    file_name.copy(packet.buf, file_name.size());
+                    
+                    sendto(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &client, i);
+                    memset(&packet, 0, sizeof(struct network_packet));
+
+                    ifstream file(arq_name);
+                    
+                    if(!list_files.is_open()){
+                        cout << "Erro ao abrir arquivo" << endl;
+                    }
+
+                    packet_count = 0;
+                    cout << "Enviando..." << endl;
+                    unsigned int bytes_read;
+
+                    auto start = high_resolution_clock::now();
+                    while(!file.eof()){
+
+                        file.read(packet.buf, BUFF_SIZE);
+                        bytes_read = file.gcount(); 
+                        packet.op = DOWNLOAD_ALL_FILES; 
+                        packet.bytes = bytes_read;
+                        packet.more = 1;
+                        
+                        sendto(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &client, i);
+                        memset(&packet, 0, sizeof(struct network_packet)); 
+
+                        packet_count++;
+                    }
+                    auto stop = high_resolution_clock::now();
+                    auto duration = duration_cast<microseconds>(stop-start);
+
+                    cout << "Arquivo enviado com sucesso!" << endl;
+                    cout << "Tempo de transmissão: " << duration.count() << " ms" << endl;
+                    cout << packet_count << " pacotes enviados " << endl;
+
+                    // Espera pro proximo arquivo
+                    usleep(150);
+
+                    packet.op = NEXT_FILE;
+                    sendto(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &client, i);
+
+                    file.close();
+                }
+
+                packet.more = 0; 
+                sendto(sockdescr, &packet, sizeof(struct network_packet), 0, (struct sockaddr *) &client, i);
+
+                auto list_stop = high_resolution_clock::now();
+                auto list_duration = duration_cast<microseconds>(list_stop-list_start);
+
+                cout << "Todos os arquivos foram enviados com sucesso!" << endl;
+                cout << "Tempo de transmissão de toda a lista: " << list_duration.count() << " ms" << endl;
+
+                list_files.close();
+                system("rm -f temp");
+
+                }
                 break;
         }
 	}
